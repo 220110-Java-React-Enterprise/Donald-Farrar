6295aa303d2a209c64d1e216d48de213fbe48268
Donald Farrar <donfar09@gmail.com>
2022-01-19 19:56:59 -0800

    updated the complete console-render

--- console-renderer/src/main/java/Main.java
+++ console-renderer/src/main/java/Main.java
@@ -1,6 +1,19 @@
 public class Main {
+    public static void main(String ...args){
+        //System.out.println("This is working!");
+        ViewManager viewManager = ViewManager.getViewManager();
 
-    public static void main(String[] args){
-        System.out.println("This is working!");
+        viewManager.registerView(new MainMenu());
+        viewManager.registerView(new SubMenu());
+
+        viewManager.navigate("MainMenu");
+
+        while(viewManager.isRunning()) {
+            viewManager.render();
+        }
+
+
     }
 }
+
+
