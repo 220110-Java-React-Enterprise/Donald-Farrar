9d9cfa85a6a7dd784d23f02f121febb7355173af
Donald Farrar <donfar09@gmail.com>
2022-01-25 13:27:03 -0800

    created constructors and getter/setters in UserModel

--- Project-0/src/main/java/Persistence/UserModel.java
+++ Project-0/src/main/java/Persistence/UserModel.java
@@ -1,4 +1,82 @@
 package Persistence;
 
 public class UserModel {
+    private Integer userId;
+    private String userName;
+    private String password;
+    private String fName;
+    private String lName;
+    private String address;
+    private Integer zip;
+
+    //Constructor
+    public UserModel() {
+    }
+
+    public UserModel(Integer userId, String userName, String password, String fName, String lName, String address, Integer zip) {
+        this.userId = userId;
+        this.userName = userName;
+        this.password = password;
+        this.fName = fName;
+        this.lName = lName;
+        this.address = address;
+        this.zip = zip;
+    }
+
+    //Getters & Setters
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getZip() {
+        return zip;
+    }
+
+    public void setZip(Integer zip) {
+        this.zip = zip;
+    }
 }
