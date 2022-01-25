a8fb264afc50c7fe22e41cbc5b66329b5fe8984a
Donald Farrar <donfar09@gmail.com>
2022-01-25 13:10:38 -0800

    added a try catch block in the main that will prink the stack trace if the code breaks. This is because I am pushing all of my exceptions to main

--- Project-0/src/main/java/Main.java
+++ Project-0/src/main/java/Main.java
@@ -1,7 +1,19 @@
+import Utils.ConnectionManager;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
 
-    public static void main(String ...args){
+    public static void main(String ...args) {
         System.out.println("This is working?");
+        try{
+            //Test the connection, will print out the stack trace if it fails
+            Connection conn = ConnectionManager.getConnection();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
 }
