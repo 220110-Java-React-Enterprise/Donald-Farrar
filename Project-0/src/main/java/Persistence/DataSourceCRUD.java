b4f038f4da71c01c7c319a67bf9c9fa06f3249e1
Donald Farrar <donfar09@gmail.com>
2022-01-25 13:04:41 -0800

    working on persistence

---
+++ Project-0/src/main/java/Persistence/DataSourceCRUD.java
@@ -0,0 +1,13 @@
+package Persistence;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+//Generic
+public interface DataSourceCRUD<T> {
+    //CRUD - create, read, update, delete
+    public Integer create(T t) throws SQLException, IOException;
+    public T read(Integer id) throws SQLException, IOException;
+    public T update(T t) throws SQLException, IOException;
+    public void delete(Integer id) throws SQLException, IOException;
+}
